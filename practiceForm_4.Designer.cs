eaa177f5d7319505e4a72c93766bb5f419e346e8
Egor Lomakin <egor_lomakin@inbox.ru>
2024-04-23 17:20:09 +0400

    #3 Добавление Практикума№4

--- practiceForm_4.Designer.cs
+++ practiceForm_4.Designer.cs
@@ -29,11 +29,290 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(practiceForm_4));
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.groupBox6 = new System.Windows.Forms.GroupBox();
+            this.buttonComplited = new System.Windows.Forms.Button();
+            this.backButton = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.task3 = new System.Windows.Forms.GroupBox();
+            this.falseAnswer32 = new System.Windows.Forms.CheckBox();
+            this.trueAnswer33 = new System.Windows.Forms.CheckBox();
+            this.falseAnswer31 = new System.Windows.Forms.CheckBox();
+            this.trueAnswer31 = new System.Windows.Forms.CheckBox();
+            this.task2 = new System.Windows.Forms.GroupBox();
+            this.task1 = new System.Windows.Forms.GroupBox();
+            this.falseAnswer1 = new System.Windows.Forms.RadioButton();
+            this.trueAnswer1 = new System.Windows.Forms.RadioButton();
+            this.falseAnswer22 = new System.Windows.Forms.RadioButton();
+            this.trueAnswer2 = new System.Windows.Forms.RadioButton();
+            this.falseAnswer21 = new System.Windows.Forms.RadioButton();
+            this.groupBox6.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.task3.SuspendLayout();
+            this.task2.SuspendLayout();
+            this.task1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolTip1
+            // 
+            this.toolTip1.IsBalloon = true;
+            this.toolTip1.ToolTipIcon = System.Windows.Forms.ToolTipIcon.Info;
+            this.toolTip1.ToolTipTitle = "Справка:";
+            // 
+            // groupBox6
+            // 
+            this.groupBox6.Controls.Add(this.buttonComplited);
+            this.groupBox6.Controls.Add(this.backButton);
+            this.groupBox6.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.groupBox6.Location = new System.Drawing.Point(0, 488);
+            this.groupBox6.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox6.Name = "groupBox6";
+            this.groupBox6.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.groupBox6.Size = new System.Drawing.Size(1101, 66);
+            this.groupBox6.TabIndex = 5;
+            this.groupBox6.TabStop = false;
+            // 
+            // buttonComplited
+            // 
+            this.buttonComplited.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonComplited.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.buttonComplited.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonComplited.ForeColor = System.Drawing.Color.White;
+            this.buttonComplited.Location = new System.Drawing.Point(928, 16);
+            this.buttonComplited.Margin = new System.Windows.Forms.Padding(4);
+            this.buttonComplited.Name = "buttonComplited";
+            this.buttonComplited.Size = new System.Drawing.Size(153, 43);
+            this.buttonComplited.TabIndex = 2;
+            this.buttonComplited.Text = "Завершить";
+            this.buttonComplited.UseVisualStyleBackColor = false;
+            this.buttonComplited.Click += new System.EventHandler(this.buttonComplited_Click);
+            // 
+            // backButton
+            // 
+            this.backButton.BackColor = System.Drawing.Color.Red;
+            this.backButton.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.backButton.ForeColor = System.Drawing.Color.White;
+            this.backButton.Location = new System.Drawing.Point(13, 16);
+            this.backButton.Margin = new System.Windows.Forms.Padding(4);
+            this.backButton.Name = "backButton";
+            this.backButton.Size = new System.Drawing.Size(128, 43);
+            this.backButton.TabIndex = 1;
+            this.backButton.Text = "Выход";
+            this.backButton.UseVisualStyleBackColor = false;
+            this.backButton.Click += new System.EventHandler(this.backButton_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.task3);
+            this.panel1.Controls.Add(this.task2);
+            this.panel1.Controls.Add(this.task1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1101, 482);
+            this.panel1.TabIndex = 4;
+            // 
+            // task3
+            // 
+            this.task3.Controls.Add(this.falseAnswer32);
+            this.task3.Controls.Add(this.trueAnswer33);
+            this.task3.Controls.Add(this.falseAnswer31);
+            this.task3.Controls.Add(this.trueAnswer31);
+            this.task3.Dock = System.Windows.Forms.DockStyle.Top;
+            this.task3.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold);
+            this.task3.Location = new System.Drawing.Point(0, 441);
+            this.task3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.task3.Name = "task3";
+            this.task3.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.task3.Size = new System.Drawing.Size(1080, 160);
+            this.task3.TabIndex = 2;
+            this.task3.TabStop = false;
+            this.task3.Text = "3. Выберите инструменты отладки";
+            // 
+            // falseAnswer32
+            // 
+            this.falseAnswer32.AutoSize = true;
+            this.falseAnswer32.Location = new System.Drawing.Point(490, 86);
+            this.falseAnswer32.Name = "falseAnswer32";
+            this.falseAnswer32.Size = new System.Drawing.Size(154, 35);
+            this.falseAnswer32.TabIndex = 3;
+            this.falseAnswer32.Text = "Архиватор";
+            this.falseAnswer32.UseVisualStyleBackColor = true;
+            // 
+            // trueAnswer33
+            // 
+            this.trueAnswer33.AutoSize = true;
+            this.trueAnswer33.Location = new System.Drawing.Point(490, 45);
+            this.trueAnswer33.Name = "trueAnswer33";
+            this.trueAnswer33.Size = new System.Drawing.Size(226, 35);
+            this.trueAnswer33.TabIndex = 2;
+            this.trueAnswer33.Text = "Профилировщик";
+            this.trueAnswer33.UseVisualStyleBackColor = true;
+            // 
+            // falseAnswer31
+            // 
+            this.falseAnswer31.AutoSize = true;
+            this.falseAnswer31.Location = new System.Drawing.Point(11, 86);
+            this.falseAnswer31.Name = "falseAnswer31";
+            this.falseAnswer31.Size = new System.Drawing.Size(257, 35);
+            this.falseAnswer31.TabIndex = 1;
+            this.falseAnswer31.Text = "Текстовый редактор";
+            this.falseAnswer31.UseVisualStyleBackColor = true;
+            // 
+            // trueAnswer31
+            // 
+            this.trueAnswer31.AutoSize = true;
+            this.trueAnswer31.Location = new System.Drawing.Point(12, 45);
+            this.trueAnswer31.Name = "trueAnswer31";
+            this.trueAnswer31.Size = new System.Drawing.Size(194, 35);
+            this.trueAnswer31.TabIndex = 0;
+            this.trueAnswer31.Text = "Дизассемблер";
+            this.trueAnswer31.UseVisualStyleBackColor = true;
+            // 
+            // task2
+            // 
+            this.task2.Controls.Add(this.falseAnswer21);
+            this.task2.Controls.Add(this.falseAnswer22);
+            this.task2.Controls.Add(this.trueAnswer2);
+            this.task2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.task2.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold);
+            this.task2.Location = new System.Drawing.Point(0, 244);
+            this.task2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.task2.Name = "task2";
+            this.task2.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.task2.Size = new System.Drawing.Size(1080, 197);
+            this.task2.TabIndex = 1;
+            this.task2.TabStop = false;
+            this.task2.Text = "2. Выберите верное: «Отладчик — это программный инструмент, позволяющий...»";
+            // 
+            // task1
+            // 
+            this.task1.Controls.Add(this.falseAnswer1);
+            this.task1.Controls.Add(this.trueAnswer1);
+            this.task1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.task1.Font = new System.Drawing.Font("Segoe UI Semibold", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.task1.Location = new System.Drawing.Point(0, 0);
+            this.task1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.task1.Name = "task1";
+            this.task1.Padding = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.task1.Size = new System.Drawing.Size(1080, 244);
+            this.task1.TabIndex = 0;
+            this.task1.TabStop = false;
+            this.task1.Text = resources.GetString("task1.Text");
+            // 
+            // falseAnswer1
+            // 
+            this.falseAnswer1.AutoSize = true;
+            this.falseAnswer1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.falseAnswer1.Location = new System.Drawing.Point(13, 180);
+            this.falseAnswer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.falseAnswer1.Name = "falseAnswer1";
+            this.falseAnswer1.Size = new System.Drawing.Size(127, 35);
+            this.falseAnswer1.TabIndex = 1;
+            this.falseAnswer1.TabStop = true;
+            this.falseAnswer1.Text = "Неверно";
+            this.falseAnswer1.UseVisualStyleBackColor = true;
+            // 
+            // trueAnswer1
+            // 
+            this.trueAnswer1.AutoSize = true;
+            this.trueAnswer1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.trueAnswer1.Location = new System.Drawing.Point(13, 139);
+            this.trueAnswer1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.trueAnswer1.Name = "trueAnswer1";
+            this.trueAnswer1.Size = new System.Drawing.Size(100, 35);
+            this.trueAnswer1.TabIndex = 0;
+            this.trueAnswer1.TabStop = true;
+            this.trueAnswer1.Text = "Верно";
+            this.trueAnswer1.UseVisualStyleBackColor = true;
+            // 
+            // falseAnswer22
+            // 
+            this.falseAnswer22.AutoSize = true;
+            this.falseAnswer22.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.falseAnswer22.Location = new System.Drawing.Point(13, 87);
+            this.falseAnswer22.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.falseAnswer22.Name = "falseAnswer22";
+            this.falseAnswer22.Size = new System.Drawing.Size(613, 35);
+            this.falseAnswer22.TabIndex = 3;
+            this.falseAnswer22.TabStop = true;
+            this.falseAnswer22.Text = "отследить сетевой трафик, генерируемый программой";
+            this.falseAnswer22.UseVisualStyleBackColor = true;
+            // 
+            // trueAnswer2
+            // 
+            this.trueAnswer2.AutoSize = true;
+            this.trueAnswer2.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.trueAnswer2.Location = new System.Drawing.Point(13, 126);
+            this.trueAnswer2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.trueAnswer2.Name = "trueAnswer2";
+            this.trueAnswer2.Size = new System.Drawing.Size(601, 35);
+            this.trueAnswer2.TabIndex = 2;
+            this.trueAnswer2.TabStop = true;
+            this.trueAnswer2.Text = "наблюдать за выполнением исследуемой программы";
+            this.trueAnswer2.UseVisualStyleBackColor = true;
+            // 
+            // falseAnswer21
+            // 
+            this.falseAnswer21.AutoSize = true;
+            this.falseAnswer21.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.falseAnswer21.Location = new System.Drawing.Point(13, 48);
+            this.falseAnswer21.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.falseAnswer21.Name = "falseAnswer21";
+            this.falseAnswer21.Size = new System.Drawing.Size(588, 35);
+            this.falseAnswer21.TabIndex = 5;
+            this.falseAnswer21.TabStop = true;
+            this.falseAnswer21.Text = "посмотреть ассемблерный код исполняемого файла";
+            this.falseAnswer21.UseVisualStyleBackColor = true;
+            // 
+            // practiceForm_4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "practiceForm_4";
+            this.ClientSize = new System.Drawing.Size(1101, 554);
+            this.ControlBox = false;
+            this.Controls.Add(this.groupBox6);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "practiceForm_4";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Практикум №4";
+            this.Load += new System.EventHandler(this.practiceForm_4_Load);
+            this.groupBox6.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.task3.ResumeLayout(false);
+            this.task3.PerformLayout();
+            this.task2.ResumeLayout(false);
+            this.task2.PerformLayout();
+            this.task1.ResumeLayout(false);
+            this.task1.PerformLayout();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.GroupBox groupBox6;
+        public System.Windows.Forms.Button buttonComplited;
+        private System.Windows.Forms.Button backButton;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.GroupBox task3;
+        private System.Windows.Forms.CheckBox falseAnswer32;
+        private System.Windows.Forms.CheckBox trueAnswer33;
+        private System.Windows.Forms.CheckBox falseAnswer31;
+        private System.Windows.Forms.CheckBox trueAnswer31;
+        private System.Windows.Forms.GroupBox task2;
+        private System.Windows.Forms.GroupBox task1;
+        private System.Windows.Forms.RadioButton falseAnswer1;
+        private System.Windows.Forms.RadioButton trueAnswer1;
+        private System.Windows.Forms.RadioButton falseAnswer21;
+        private System.Windows.Forms.RadioButton falseAnswer22;
+        private System.Windows.Forms.RadioButton trueAnswer2;
     }
 }
