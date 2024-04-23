989633a56a5bc3ca03ba5d9153aafb2456434ce8
Egor Lomakin <egor_lomakin@inbox.ru>
2024-04-23 18:19:19 +0400

    #3 Добавление Практикума№6

--- practiceForm_6.cs
+++ practiceForm_6.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace DebuggerHandbook
@@ -15,6 +8,75 @@ namespace DebuggerHandbook
         public practiceForm_6()
         {
             InitializeComponent();
+        }
+
+        private void practiceForm_6_Load(object sender, EventArgs e)
+        {
+            toolTip1.SetToolTip(backButton, "Выйти на главную форму.");
+            toolTip1.SetToolTip(buttonComplited, "Завершить практикум.");
+        }
+
+
+        private int totalScore = 0;
+
+        private void CalculateResult()
+        {
+            if (trueAnswer1.Checked) totalScore++;
+            if (trueAnswer21.Checked && trueAnswer22.Checked) totalScore++;
+            if (trueAnswer31.Checked) totalScore++;
+        }
+
+        private void buttonComplited_Click(object sender, EventArgs e)
+        {
+            CalculateResult();
+
+            if (totalScore == 3)
+            {
+                DialogResult messageResult = MessageBox.Show(
+                    $"Вы ответили верно на все вопросы.\nТак держать!",
+                    "Практикум №6 завершен!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1
+                    );
+
+                if (messageResult == DialogResult.OK)
+                {
+                    this.DialogResult = DialogResult.Yes;
+                    this.Close();
+                }
+            }
+            else
+            {
+                DialogResult messageResult = MessageBox.Show(
+                    $"Правильных ответов {totalScore} из 3.\n" +
+                    $"Повторите теоретический материал и у вас всё получится!",
+                    "К сожалению, вы не прошли практикум.",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Information,
+                    MessageBoxDefaultButton.Button1
+                    );
+
+                if (messageResult == DialogResult.OK)
+                {
+                    this.DialogResult = DialogResult.No;
+                    this.Close();
+                }
+            }
+        }
+
+        private void backButton_Click(object sender, EventArgs e)
+        {
+            DialogResult messageResult = MessageBox.Show(
+                "Вы точно хотите выйти?\nВ случае выхода прогресс выполнения практики будет утерян!",
+                "Выход",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Warning,
+                MessageBoxDefaultButton.Button2
+                );
+
+            if (messageResult == DialogResult.Yes)
+                this.Close();
         }
     }
 }
